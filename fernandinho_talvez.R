3f1901afd7dea89e27af9b6cbeee50545455e6ea
Mariana <mrq.vet@gmail.com>
2014-06-27 11:06:23 -0300

    lembrar o que fiz com Fernandinho

---
+++ fernandinho_talvez.R
@@ -0,0 +1,129 @@
+####### o que fiz com o Fernandinho ta por aqui
+summary(rede_circuitos)
+rede_circuitos <- graph.data.frame(gtas.coords,directed=T)
+library(igraph)
+rede_circuitos <- graph.data.frame(gtas.coords,directed=T)
+length(ordem_cod_grafo)
+str(rede_circuitos)
+rede_circuitos <- graph.data.frame(redes,directed=T)
+summary(rede_circuitos)
+rede_circuitos
+rede_circuitos[1]
+rede_circuitos$attr
+rede_circuitos$vertex
+rede_circuitos[[1]]
+rede_circuitos[[1]]$513
+rede_circuitos[[1]]
+View(redes)
+ordem_cod_grafo <- get.vertex.attribute(graph=rede_circuitos,name='name')
+ordem_cod_grafo
+ordem_cod_grafo[5552]
+summary(ordem_cod_grafo)
+View(coord.limp)
+typeof(coord.limp$CODIGO_PROPRIEDADE)
+as.numeric(ordem_cod_grafo[1])
+head(ordem_cod_grafo)
+which(coord.limp$CODIGO_PROPRIEDADE == as.numeric(ordem_cod_grafo[i])
+)
+which(coord.limp$CODIGO_PROPRIEDADE == as.numeric(ordem_cod_grafo[1]))
+which(coord.limp$CODIGO_PROPRIEDADE == as.numeric(ordem_cod_grafo[2]))
+which(coord.limp$CODIGO_PROPRIEDADE == as.numeric(ordem_cod_grafo[3]))
+which(coord.limp$CODIGO_PROPRIEDADE == as.numeric(ordem_cod_grafo[5555]))
+which(coord.limp$CODIGO_PROPRIEDADE == as.numeric(ordem_cod_grafo[55555]))
+which(coord.limp$CODIGO_PROPRIEDADE == as.numeric(ordem_cod_grafo[555555]))
+head(ordem_cod_grafo)
+which(ordem_cod_grafo==43060560003)
+which(ordem_cod_grafo != 43060560003)
+which(ordem_cod_grafo == as.character(43060560003))
+as.character(43060560003)
+which(ordem_cod_grafo == as.character(513))
+head(ordem_cod_grafo)
+which(coord.limp$CODIGO_PROPRIEDADE == (513))
+which(coord.limp$CODIGO_PROPRIEDADE == (43060560003))
+which(redes$Out == coord.limp$CODIGO_PROPRIEDADE)
+for (i in length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos[i] <- which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i])
+}
+for (i in length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos <- c(limpos,which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i]))
+}
+limpos <- numeric()
+limpos
+for (i in length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos <- c(limpos,which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i]))
+}
+limpos
+typeof(redes$Out)
+typeof(coord.limp$CODIGO_PROPRIEDADE)
+limpos <- NULL
+for (i in length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos <- c(limpos,which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i]))
+}
+limpos
+length(coord.limp$CODIGO_PROPRIEDADE)
+limpos <- numeric()
+for (i in 1:length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos <- c(limpos,which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i]))
+}
+limpos
+limpos <- numeric()
+for (i in 1:length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos[i]  <- which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i]))
+}
+limpos <- NULL
+for (i in 1:length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos[i]  <- which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i]))
+}
+for (i in 1:length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos[i]  <- which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i])
+}
+limpos
+i
+coord.limp$CODIGO_PROPRIEDADE[i]
+redes$Out
+limpos  <- which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i])
+limpos  <- which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i])
+limpos
+limpos  <- which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i])
+which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i])
+coord.limp$CODIGO_PROPRIEDADE[i]
+which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i])
+limpos  <- which(redes$In == coord.limp$CODIGO_PROPRIEDADE[i])
+limpos
+limpos  <- which(redes$In == coord.limp$CODIGO_PROPRIEDADE[1:10000])
+limpos  <- which(redes$In == coord.limp$CODIGO_PROPRIEDADE[10000])
+limpos
+limpos  <- which(redes$Out == 513)
+limpos
+limpos  <- c(limpos,which(redes$Out == 513))
+limpos
+head(redes$Out)
+limpos <- NUll
+limpos <- NULL
+limpos  <- c(limpos,which(redes$Out == 513))
+limpos
+head(redes$Out)
+limpos  <- c(limpos,which(redes$Out == 148178))
+limpos
+redesFer <- redes[limpos,]
+redesFer
+limpos <- NULL
+for (i in 1:length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos  <- c(limpos,which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i]))
+}
+limpos
+limpos <- NULL
+for (i in 1:length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos  <- c(limpos,which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i]))
+  print(i)
+}
+limpos <- NULL
+for (i in 1:length(coord.limp$CODIGO_PROPRIEDADE)){
+  limpos  <- c(limpos,which(redes$Out == coord.limp$CODIGO_PROPRIEDADE[i]))
+  limpos  <- c(limpos,which(redes$In == coord.limp$CODIGO_PROPRIEDADE[i]))
+  print(i)
+}
+limpos[[1]]
+limpos[[15]]
+View(rede2013)
+View(rede2012)
